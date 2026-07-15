113b9c4157554281f77a5ff538bf0a758c449428
ledangquangdangquang <quang.ld224113@sis.hust.edu.vn>
2026-07-15 14:48:56 +0700

    screenshot

--- home.nix
+++ home.nix
@@ -35,6 +35,60 @@
           --bind 'enter:become(nvim -- {})'
     '';
   };
+
+  screenshot = pkgs.writeShellApplication {
+    name = "screenshot";
+    runtimeInputs = with pkgs; [
+      coreutils
+      grim
+      libnotify
+      satty
+      slurp
+      wl-clipboard
+    ];
+    text = ''
+      mode="''${1:-region}"
+      screenshot_dir="''${HOME}/Pictures/Screenshots"
+      screenshot_file="''${screenshot_dir}/Screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
+
+      mkdir -p "$screenshot_dir"
+
+      select_region() {
+        slurp -d -b '#1e1e2ecc' -c '#cba6f7ff' -s '#31324488' -w 2
+      }
+
+      edit_screenshot() {
+        satty \
+          --filename - \
+          --fullscreen \
+          --output-filename "$screenshot_file" \
+          --copy-command wl-copy
+      }
+
+      case "$mode" in
+        region)
+          geometry="$(select_region)" || exit 0
+          grim -g "$geometry" -t ppm - | edit_screenshot
+          ;;
+        full)
+          grim -t ppm - | edit_screenshot
+          ;;
+        copy)
+          geometry="$(select_region)" || exit 0
+          grim -g "$geometry" -t png - | wl-copy --type image/png
+          notify-send \
+            --app-name="Screenshot" \
+            --expire-time=2500 \
+            "Screenshot copied" \
+            "The selected region is in the clipboard"
+          ;;
+        *)
+          echo "Usage: screenshot {region|full|copy}" >&2
+          exit 2
+          ;;
+      esac
+    '';
+  };
 in {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
@@ -76,6 +130,9 @@ in {
     bat
     git
     fuzzel
+    grim
+    slurp
+    satty
     swaynotificationcenter
     libnotify
     fastfetch
@@ -99,6 +156,7 @@ in {
     ddcutil # brightness
     eza # alternative ls
     fuzzyvim
+    screenshot
     (qt6Packages.fcitx5-with-addons.override {
       addons = [
         qt6Packages.fcitx5-unikey
