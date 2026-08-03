8a2bd54a71e4ab3b5b30a089acb18b97a5db7c6d
ledangquangdangquang <quang.ld224113@sis.hust.edu.vn>
2026-08-03 13:25:34 +0700

    instruction keyshortcut mod I

--- dotfiles/i3/show-keyshortcuts.sh
+++ dotfiles/i3/show-keyshortcuts.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-fuzzel \
-	--dmenu \
-	--prompt="Shortcuts (Esc to close) > " \
-	--lines=25 \
-	--width=64 \
+rofi \
+	-dmenu \
+	-i \
+	-p "Shortcuts (Esc to close) > " \
+	-lines 25 \
+	-width 64 \
+	-theme "$HOME/.config/rofi/catppuccin-mocha.rasi" \
 	<"$HOME/.config/i3/keyshortcuts.txt" \
 	>/dev/null
